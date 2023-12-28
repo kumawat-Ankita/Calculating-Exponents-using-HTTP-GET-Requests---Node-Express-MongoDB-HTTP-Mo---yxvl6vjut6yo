f7131e8a1f8b9c048c3c70dddf19962c87f9c04a
kumawat-Ankita <145592877+kumawat-Ankita@users.noreply.github.com>
2023-12-28 18:49:39 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,36 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST') {
-    const chunks = [];
+  if (req.method === 'POST' && req.url === '/calculate') {
+    let body = '';
 
     req.on('data', chunk => {
-      const buf = Buffer.from(chunk);
-      const str = buf.toString();
-      chunks.push(str);
-      const obj = JSON.parse(chunks)
-      const value1 = obj.num1;
-      const value2 = obj.num2;
-
-      if(value1 <=0  || value2 <0){
-        res.writeHead(404,{"Content-Type": "text/plain"});
-        res.end("The operation cannot be performed");
-      }else{
-        res.writeHead(200,{"Content-Type": "text/plain"});
-        res.end(Math.pow(value1 ,value2));
-      }
-      
+      body += chunk.toString(); // Collecting the incoming data
     });
-    }
+
+    req.on('end', () => {
+      try {
+        const data = JSON.parse(body); // Parsing the complete payload as JSON
+        const value1 = parseInt(data.num1);
+        const value2 = parseInt(data.num2);
+
+        if (isNaN(value1) || isNaN(value2) || value1 <= 0 || value2 < 0) {
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          res.end("The operation cannot be performed");
+        } else {
+          const result = Math.pow(value1, value2);
+          res.writeHead(200, { "Content-Type": "text/plain" });
+          res.end(result.toString()); // Sending the result as a string
+        }
+      } catch (error) {
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end("Invalid input data");
+      }
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Invalid endpoint or method");
+  }
 });
 
 module.exports = server;
-      
